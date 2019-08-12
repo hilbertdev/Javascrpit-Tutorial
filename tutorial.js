67aeae3b18f112dbbae5d51c2e554d80815b8316
Hilbert <Hilbertmuna@gmail.com>
2019-08-12 17:01:41 +0200

    javascrpit tut

---
+++ tutorial.js
@@ -0,0 +1,17 @@
+// var , let , const 
+//var is globally scoped 
+//let you can reassign values 
+//const means it cannot be changed 
+//always use const unless you know you are gonna reassign it to make your code more robust 
+//Data types => Strings, numbers, boolean, null, undefined
+
+const name = 'John';
+const num = 19;
+const isCool = true; 
+const rating = 4.5; //there is not float data type in js, it is just a number
+const x = null; 
+const y = undefined; 
+console.log(typeof y);
+
+
+//concatenation 
