064b7776da8e705ec57c52407fdd08c909ba09a6
Hilbert <Hilbertmuna@gmail.com>
2019-08-17 19:00:07 +0200

    cleaner code

--- todo.js
+++ todo.js
@@ -1,20 +1,19 @@
 //variables 
-var id = 0; 
-var listItem; 
+var id = 0;  
 var taskArray = [];
 var completedList =[]; 
 //var checkbox; 
 
 //Const variables
-const ul = document.querySelector('.items');
+const todoListitems = document.querySelector('.toDolistitems');
 const done = document.querySelector('.done');
-const textBox = document.querySelector('#task');
-const btn = document.querySelector('.btn');
-const msg = document.querySelector('.msg');
-const refreshbtn = document.querySelector('.refresh');
+const taskEntered = document.querySelector('#task');
+const btnAdd = document.querySelector('.btnAdd');
+const errMsg = document.querySelector('.msg');
+const btnClear = document.querySelector('.btnClear');
 //
 
-class toDo {  
+class listItem {  
     constructor(id, task, isComplete) {
         this.id = id;
         this.task = task; 
@@ -23,80 +22,129 @@ class toDo {
     incrementID(){
         id++;
     }
-    addToList(task){
-        var checkbox = document.createElement("input");
+   
+}
+
+//adding an entered task to the displayed list 
+function addToList(task) {
+    var checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.name = "checkbox"; 
-        var str = task;
-        var str= str.trim()
-        checkbox.value = str;
-        //checkbox.className = "checkbox";
-      
+        checkbox.name = id.toString(); 
+        checkbox.value = task;
         var textNode = document.createTextNode(task);
         var span = document.createElement('span');
-        span.className =textNode.nodeValue.trim();
+        span.className =textNode.nodeValue;
         span.innerHTML = textNode.nodeValue;
         var breakpoint = document.createElement("br");
-        ul.appendChild(checkbox);
-        ul.appendChild(span);
-        ul.appendChild(breakpoint);
-        
-      
+        todoListitems.appendChild(checkbox);
+        todoListitems.appendChild(span);
+        todoListitems.appendChild(breakpoint);
+}
+//                      END                         ///
+
+//This is the event handler for adding a task into the todo list 
+btnAdd.addEventListener('click', (e) => {
+    if(document.querySelector('#task').value === ''){
+        errMsg.innerHTML = 'Please Enter A task'
+        taskEntered.style.borderColor = 'red';
     }
- 
+    else {
+      let item = document.querySelector('#task').value;
+      if(!checkifAdded(taskArray,item)) {
+          console.log(taskArray);
+      var task = new listItem(id,item, false);
+      taskArray[id] = task; 
+      task.incrementID();
+      addToList(task.task);
+      btnClear.style.display = "inline"
+      taskEntered.value = '';
+      }
+      else {
+        errMsg.innerHTML = 'Item exists'; 
+      }
+    }
+});
+
+function checkifAdded(arr, value ) {
+   for(i = 0; i < arr.length; i++){
+       if(arr[i].task === value)
+       {
+           if(arr[i].isComplete === false){
+            return true;
+           }
+           
+       }
+   }
+   return false; 
+}
+function searchItem(arr, val, updateBool) {
+        for(i = 0; i < arr.length; i++){
+            if(arr[i].task === val)
+            {
+             arr[i].isComplete = updateBool;
+             
+             return arr[i].isComplete; 
+            }
+        }
+    return -1;
 }
 
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(document.querySelector('#task').value === ''){
-       msg.innerHTML = 'Please Enter A task'
-        textBox.style.borderColor = 'red';
-    }
-    else {
-      listItem = document.querySelector('#task').value;
-      var task = new toDo(id,listItem, false);
-      taskArray[id] = task; 
-      task.incrementID();
-      task.addToList(listItem);
-      refreshbtn.style.display = "inline"
-      textBox.value = '';
-    }
+//                      END                         //
+
+//Cross Out item if completed 
+
+//                      END                         //
+
+
+btnClear.addEventListener('click', (e) => {
+    mapToCompleted(updateCompleted());
+    updateToDo();
 });
 
-refreshbtn.addEventListener('click', (e) => {
+function updateToDo(){
+    for(i = 0; i < taskArray.length; i++){
+        document.querySelector('.mainList').innerHTML = ''; 
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name =  taskArray[i].id.toString(); 
+        checkbox.value = taskArray[i].task;
+        var textNode = document.createTextNode(taskArray[i].task);
+        var span = document.createElement('span');
+        span.className =textNode.nodeValue;
+        span.innerHTML = textNode.nodeValue;
+        var breakpoint = document.createElement("br");
+        todoListitems.appendChild(checkbox);
+        todoListitems.appendChild(span);
+        todoListitems.appendChild(breakpoint);
+    }
+}
+
+function updateCompleted(){
     var checkboxNode = document.querySelectorAll('input[name=checkbox]'); 
-    var i = 0; 
     checkboxNode.forEach((checkitem) => {
-          if(checkitem.checked){
-            checkitem.type = 'hidden';
-            checkitem.nextElementSibling.remove();
-            var updatedList = taskArray; 
-            updatedList.filter((namedTask) => {
-                if(namedTask.task === checkitem.value && completedList.indexOf(namedTask.task) < 0){
-                    namedTask.isComplete = true;
-                    completedList[i++] = namedTask.task; 
-                }
-            })
-          }
-          return completedList;
-    })
-    
-    mapToCompleted(completedList);
-});
+        if(checkitem.checked){
+          checkitem.type = 'hidden';
+          checkitem.nextElementSibling.remove();
+          var checkeditem = searchItem(taskArray ,checkitem.nodeValue, true);
+          completedList.push(checkeditem);
+          //var updatedList = taskArray; 
+        }
+        ;
+  })
+  return completedList;
+}
 
 function mapToCompleted (completedArray){
     for(i = 0; i < completedArray.length; i++){
-        var textNode = document.createTextNode(completedArray[i]);
-        var strike = document.createElement('strike');
-        strike.innerText = textNode.nodeValue;
-        var breakpoint = document.createElement("br");
-        done.appendChild(strike);
-        //done.appendChild(textNode);
-        done.appendChild(breakpoint);
-        //refreshbtn.style.display = "none"
+        var complete = document.createTextNode(completedArray[i].task);
+        var itemC = document.createElement('li');
+        var br = document.createElement('br');
+        itemC.innerHTML = complete.nodeValue;
+        itemC.innerHTML.strike();
+        done.appendChild(itemC);
+        done.appendChild(br);
     }
-  
 }
 
  
